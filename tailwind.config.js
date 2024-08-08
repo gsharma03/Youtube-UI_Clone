c8a6ec40dba4fcb67c0185ccbfc6eadc4fffd5f1
gsharma03 <gauravsh.9315@gmail.com>
2024-08-08 12:04:42 +0530

    adjusted code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./dist/*.html"],
+  content: ["./docs/*.html"],
   theme: {
     extend: {
       fontFamily: {
